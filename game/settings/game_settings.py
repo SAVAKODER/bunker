0110ca1ac4d83b89df64184c5eb3eea888f68e0c
danil <trrripled@vk.com>
2023-08-20 19:26:33 +0800

    new path for JSON_CHARACTERISTICS_DIRECTORY

--- game/settings/game_settings.py
+++ game/settings/game_settings.py
@@ -2,4 +2,4 @@ import pathlib
 
 
 BASE_DIRECTORY = pathlib.Path(__file__).absolute().parent.parent
-JSON_CHARACTERISTICS_DIRECTORY = BASE_DIRECTORY / 'game_infrastructure'
+JSON_CHARACTERISTICS_DIRECTORY = BASE_DIRECTORY / 'game_infrastructure' / 'json'
